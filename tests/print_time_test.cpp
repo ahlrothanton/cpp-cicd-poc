26a652f8bc727807cc8107b4b7b0e4a98b7f77e4
Anton Ahlroth <ahlrothanton@gmail.com>
2024-02-14 22:20:39 +0200

    Adds regex matching for the timestamp format.

--- tests/print_time_test.cpp
+++ tests/print_time_test.cpp
@@ -15,13 +15,24 @@ TEST(PrintTimeTest, DefaultFormat) {
     std::string output = testing::internal::GetCapturedStdout();
     EXPECT_TRUE(output.find(prefix) == 0);
     std::cout << output << std::endl;
+
+    // regex pattern for default format
+    std::string ts = output.substr(prefix.size());
+    std::regex pattern(R"(([A-Za-z]{3} [A-Za-z]{3} \d{2} \d{2}:\d{2}:\d{2} \d{4})\n)");
+    ASSERT_TRUE(std::regex_match(ts, pattern)) << "Input does not match the pattern: " << ts;
+
 }
 
 // test print time with custom format
 TEST(PrintTimeTest, CustomFormat) {
     testing::internal::CaptureStdout();
-    printCurrentTime("%u");
+    printCurrentTime("%s");
     std::string output = testing::internal::GetCapturedStdout();
     EXPECT_TRUE(output.find(prefix) == 0);
     std::cout << output << std::endl;
+
+    // regex pattern for Unix timestamp
+    std::string ts = output.substr(prefix.size());
+    std::regex pattern("^\\s*\\d+\\s*$");
+    ASSERT_TRUE(std::regex_match(ts, pattern)) << "Input does not match the pattern: " << ts;
 }
